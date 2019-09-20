d20470f010a8f1a0943ef40da581e2c90bbad42e
Jak <rekax5@gmail.com>
2019-09-20 20:29:57 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -8,41 +8,47 @@ public class Main {
         int second;
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Введите задачу: ");
+        System.out.println("Введите пример с арабскими или римскими цифрами от 1 до 10: ");
         String str = sc.nextLine();
-        System.out.println("ваша строка: " + str);
+
+        System.out.println("введенная  строка: " + str);
 
         int slojenie = str.indexOf('+');
         int vichitanie = str.indexOf('-');
         int umnojenie = str.indexOf('*');
         int delenie = str.indexOf('/');
-//        System.out.println("slojenie= "+slojenie);
-//        System.out.println("vichitanie"+vichitanie);
-//        System.out.println("umnojenie "+umnojenie);
-//        System.out.println("delenie "+delenie);
+
         if (slojenie > 0) {
             String newPart[] = str.split("\\+");
             int firstPart = Integer.parseInt(newPart[0]);
             int secondPart= Integer.parseInt(newPart[1]);
 
-            System.out.println("1 часть " + firstPart);
-            System.out.println("2 часть " + secondPart);
+            System.out.println(Operations.Slojenie(firstPart,secondPart));
+            System.out.println(newPart[0].equalsIgnoreCase("I"));
         }
 
         if (vichitanie > 0) {
             String newPart[] = str.split("\\-");
+            int firstPart = Integer.parseInt(newPart[0]);
+            int secondPart= Integer.parseInt(newPart[1]);
+            System.out.println(Operations.Vichitanie(firstPart,secondPart));
 
-            System.out.println("1 часть " + newPart[0]);
-            System.out.println("2 часть " + newPart[1]);
+
         }
 
 
         if (umnojenie > 0) {
             String newPart[] = str.split("\\*");
+            int firstPart = Integer.parseInt(newPart[0]);
+            int secondPart= Integer.parseInt(newPart[1]);
+            System.out.println(Operations.Umnojenie(firstPart,secondPart));
         }
 
         if (delenie > 0) {
             String newPart[] = str.split("\\/");
+            int firstPart = Integer.parseInt(newPart[0]);
+            int secondPart= Integer.parseInt(newPart[1]);
+            System.out.println(Operations.Delenie(firstPart,secondPart));
         }
 
 
@@ -50,31 +56,11 @@ public class Main {
 
 
 
-        char myArray[] = str.toCharArray();
-        char firstZnachenie = myArray[0];
-
-        int number1 = Character.getNumericValue(firstZnachenie);
-        char secondZnachenie = myArray[2];
-        int number2 = Character.getNumericValue(secondZnachenie);
 
 
-        if (slojenie > 0) {
 
-            System.out.println(Operations.Slojenie(number1, number2));
 
-        }
-        if (vichitanie > 0) {
-            System.out.println(Operations.Vichitanie(number1, number2));
 
-        }
-        if (umnojenie > 0) {
-            System.out.println(Operations.Umnojenie(number1, number2));
-
-        }
-        if (delenie > 0) {
-            System.out.println(Operations.Delenie(number1, number2));
-
-        }
 
 
     }
