b465daceedb571f2e2640372bafbf64d66af04cc
Jak <rekax5@gmail.com>
2019-09-22 19:16:34 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,10 @@ public class Main {
 
 
     public static void main(String[] args) {
-        int indexZnakOperation;
-        int second;
+        int indexZnakOperation=0;
+        int roman1=0;
+        int roman2=0;
+        String a;
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите пример с арабскими или римскими цифрами от 1 до 10: ");
@@ -13,56 +15,77 @@ public class Main {
 
         System.out.println("введенная  строка: " + str);
 
+
         int slojenie = str.indexOf('+');
-        int vichitanie = str.indexOf('-');
-        int umnojenie = str.indexOf('*');
-        int delenie = str.indexOf('/');
 
-        if (slojenie > 0) {
-            String newPart[] = str.split("\\+");
-            int firstPart = Integer.parseInt(newPart[0]);
-            int secondPart= Integer.parseInt(newPart[1]);
+        String newPart[] = str.split("\\+");
+//        int firstPart = Integer.parseInt(newPart[0]);
+//        int secondPart = Integer.parseInt(newPart[1]);
 
-            System.out.println(Operations.Slojenie(firstPart,secondPart));
-            System.out.println(newPart[0].equalsIgnoreCase("I"));
-        }
-
-        if (vichitanie > 0) {
-            String newPart[] = str.split("\\-");
-            int firstPart = Integer.parseInt(newPart[0]);
-            int secondPart= Integer.parseInt(newPart[1]);
-            System.out.println(Operations.Vichitanie(firstPart,secondPart));
+        if (newPart[0].equals("I")||(newPart[0].equals("II"))||(newPart[0].equals("III"))||(newPart[0].equals("IV"))||
+                (newPart[0].equals("V"))||(newPart[0].equals("VI"))||(newPart[0].equals("VII"))||
+                (newPart[0].equals("VIII"))||(newPart[0].equals("IX"))||(newPart[0].equals("X"))){
 
 
-        }
+            System.out.println("парт ноль "+roman1);
+        } else roman1=1;
+        System.out.println("иначе "+roman1);
+
+        if (newPart[1].equals("I")||(newPart[1].equals("II"))||(newPart[1].equals("III"))||(newPart[1].equals("IV"))||
+                (newPart[1].equals("V"))||(newPart[1].equals("VI"))||(newPart[1].equals("VII"))||
+                (newPart[1].equals("VIII"))||(newPart[1].equals("IX"))||(newPart[1].equals("X"))){
 
 
-        if (umnojenie > 0) {
-            String newPart[] = str.split("\\*");
-            int firstPart = Integer.parseInt(newPart[0]);
-            int secondPart= Integer.parseInt(newPart[1]);
-            System.out.println(Operations.Umnojenie(firstPart,secondPart));
-        }
-
-        if (delenie > 0) {
-            String newPart[] = str.split("\\/");
-            int firstPart = Integer.parseInt(newPart[0]);
-            int secondPart= Integer.parseInt(newPart[1]);
-            System.out.println(Operations.Delenie(firstPart,secondPart));
-        }
+            System.out.println(roman2);
+        } else roman2=1;
+        System.out.println("иначе "+ roman2);
+        if (roman1==roman2) {
+            System.out.println("выполняем операцию");
+        } else
+            PrintMassage.PrintWarning();
 
 
 
-
-
-
-
-
-
-
-
+//        int vichitanie = str.indexOf('-');
+//        int umnojenie = str.indexOf('*');
+//        int delenie = str.indexOf('/');
+//
+//        if (slojenie > 0) {
+//            String newPart[] = str.split("\\+");
+//            int firstPart = Integer.parseInt(newPart[0]);
+//            int secondPart= Integer.parseInt(newPart[1]);
+//
+//            System.out.println(Operations.Slojenie(firstPart,secondPart));
+//            System.out.println(newPart[0].equalsIgnoreCase("I"));
+//        }
+//
+//        if (vichitanie > 0) {
+//            String newPart[] = str.split("\\-");
+//            int firstPart = Integer.parseInt(newPart[0]);
+//            int secondPart= Integer.parseInt(newPart[1]);
+//            System.out.println(Operations.Vichitanie(firstPart,secondPart));
+//
+//
+//        }
+//
+//
+//        if (umnojenie > 0) {
+//            String newPart[] = str.split("\\*");
+//            int firstPart = Integer.parseInt(newPart[0]);
+//            int secondPart= Integer.parseInt(newPart[1]);
+//            System.out.println(Operations.Umnojenie(firstPart,secondPart));
+//        }
+//
+//        if (delenie > 0) {
+//            String newPart[] = str.split("\\/");
+//            int firstPart = Integer.parseInt(newPart[0]);
+//            int secondPart= Integer.parseInt(newPart[1]);
+//            System.out.println(Operations.Delenie(firstPart,secondPart));
+//        }
 
 
     }
+    }
 
-}
+
+
---
+++ src/PrintMassage.java
@@ -0,0 +1,13 @@
+public class PrintMassage {
+    void PrintMassage() {
+
+    }
+
+    public static void PrintWarning(){
+        System.out.println("Введите корректные данные!!!");
+    }
+    public void PrintWarning2(){
+        System.out.println("Введите римские числа!!!");
+    }
+
+}
